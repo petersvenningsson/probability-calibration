1581950781ca147120c7fcb4eb0a13b3bb0b6089
Peter Svenningsson <peter.o.svenningsson@gmail.com>
2021-09-19 18:45:43 +0200

    Resukts

--- simconfig.py
+++ simconfig.py
@@ -1,3 +1,4 @@
+import matplotlib as plt
 
 # simdataset
 mean_A = [0.5, -0.5]
@@ -11,3 +12,8 @@ train_dataset_samples = 100000
 test_dataset_samples = 10000
 goal_entropy = 0.5 # maximum entropy is log(2) approx 0.7.
 goal_accuracy = 0.75
+
+plt.rcParams["figure.figsize"] = (4, 4) 
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = ["Times New Roman"] + plt.rcParams["font.serif"]
+plt.rcParams["mathtext.fontset"] = "cm"
